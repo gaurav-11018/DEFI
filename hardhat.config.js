42d773497a875ce8cd5caca5853556d1b12da925
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-07-13 22:20:21 +0530

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,19 +9,29 @@ const RINKEBY_RPCURL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 31337,
       forking: {
-        url: MAINNET_RPC_URL,
+        url: "https://eth-mainnet.g.alchemy.com/v2/mAA_GIR4tODHsi20aIKGsKk0mlr9iJGm",
       },
-    },
-    localhost: {
       chainId: 31337,
+      // forking: {
+      //   url: MAINNET_RPC_URL,
+      // },
     },
+    // localhost: {
+    //   chainId: 31337,
+    // },
+    // kovan: {
+    //   url: KOVAN_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 42,
+    //   blockConfirmations: 6,
+    // },
   },
   solidity: {
     compilers: [
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,16 @@
+const { getNamedAccounts } = require("hardhat");
 const { getWeth } = require("../scripts/getWeth");
 
 async function main() {
   await getWeth();
+  const { deployer } = await getNamedAccounts();
+}
+//   Got 0.20000000000000000ETH
+//   Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+//   Lending Pool: ^
+
+async function(){
+    
 }
 
 main()
