8fc7921b4aa473498c0d2cc2fe2b4548ff889fe2
gaurav-11018 <79459872+gaurav-11018@users.noreply.github.com>
2022-07-18 23:40:36 +0530

    Add files via upload

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -4,18 +4,22 @@ const { getWeth } = require("../scripts/getWeth");
 async function main() {
   await getWeth();
   const { deployer } = await getNamedAccounts();
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`LendingPool address ${lendingPool.address}`);
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 }
 //   Got 0.20000000000000000ETH
 //   Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
 //   Lending Pool: ^
 
 async function getLendingPool(account) {
-  const lendingPoolAddressProvider = await ethers.getContract(
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
     "ILendingPoolAddressesProvider",
     "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
     account
   );
-  const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool();
+  const lendingPoolAddress =
+    await lendingPoolAddressesProvider.getLendingPool();
   const lendingPool = await ethers.getContractAt(
     "ILendingPool",
     lendingPoolAddress,
