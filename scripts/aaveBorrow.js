98724539c302c2b6299dc65ea952af236cd1f9de
Gaurav <79459872+gaurav-11018@users.noreply.github.com>
2022-07-21 02:05:06 +0530

    Add files via upload

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -11,20 +11,66 @@ async function main() {
   console.log("Depositing........");
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
   console.log("Deposited!");
-  let { availbleBorrowsETH, totalDebtETH } = await getBorrowUserData(
+  let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
     lendingPool,
     deployer
   );
+
+  const daiPrice = await getDaiPrice();
+  const amountDaiTOBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber());
+  console.log(`you can borrow ${amountDaiTOBorrow} DAI`);
+  const amountDaiTOBorrowWei = ethers.utils.parseEther(
+    amountDaiTOBorrow.toString()
+  );
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiTOBorrowWei, deployer);
+  await getBorrowUserData(lendingPool, deployer);
+  await repay(amountDaiTOBorrowWei, daiTokenAddress, lendingPool, deployer);
 }
-//   Got 0.20000000000000000ETH
-//   Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
-//   Lending Pool: ^
+// Got 0.20000000000000000ETH
+// Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+// Lending Pool: ^
+
+async function repay(amount, daiAddress, lendingPool, account) {
+  await approveErc20(daiAddress, lendingPool.address, amount, account);
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account);
+  await repayTx.wait(1);
+  console.log("Repaid");
+}
+
+async function borrowDai(
+  daiAddress,
+  lendingPool,
+  amountDaiTOBorrowWei,
+  account
+) {
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiTOBorrowWei,
+    1,
+    0,
+    account
+  );
+  await borrowTx.wait(1);
+  console.log("You've borrowed!");
+}
+
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  );
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+  return price;
+}
 
 async function getBorrowUserData(lendingPool, account) {
   const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
     await lendingPool.getUserAccountData(account);
   console.log(`You have ${totalCollateralETH} worth of ETH deposited. `);
-  console.log(`You have ${totalDebtETH} worth of ETH borrowed. `);
+  console.log(`You hvae ${totalDebtETH} worth of ETH borrowed. `);
   console.log(`You can borrow ${availableBorrowsETH} worth of ETH . `);
   return { availableBorrowsETH, totalDebtETH };
 }
